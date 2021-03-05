eb0ffe2062b3ba6ad0d5d3a00004e768913cd6ed
Brian Palmer <bpalmer710@gmail.com>
2021-03-05 13:22:35 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+
+def input_to_index(user_input)
+  user_input.to_i-1 
+  
+end
+
+board = [" ", " ", " "]
+def move (board, index, value= "X")
+  board[index]= value
+end
